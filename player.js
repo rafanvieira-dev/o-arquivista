790cf4e9732ef79bc0995119cbd1a10bff8166f9
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 18:43:30 -0300

    Update player.js

--- player.js
+++ player.js
@@ -3,23 +3,20 @@ class Player {
         this.x = x;
         this.y = y;
         
-        // Tamanho do personagem no jogo
         this.width = 64;  
         this.height = 85; 
         
-        // Físicas
+        // Físicas (Mais ágeis e menos "flutuantes")
         this.vx = 0;
         this.vy = 0;
-        this.speed = 4;
-        this.runSpeed = 7;
-        this.jumpForce = -12;
-        this.gravity = 0.6;
+        this.speed = 5;       
+        this.runSpeed = 8;    
+        this.jumpForce = -15; 
+        this.gravity = 0.8;   
         this.grounded = false;
         
-        // Direção (1 = direita, -1 = esquerda)
         this.facing = 1; 
 
-        // --- SISTEMA DE SPRITES ---
         this.image = new Image();
         this.image.src = 'assets/sprites/arquivista.png'; 
         
@@ -27,14 +24,12 @@ class Player {
         this.frameY = 0; 
         this.maxFrame = 3; 
         
-        // Controle de tempo da animação
         this.fps = 8; 
         this.frameInterval = 1000 / this.fps;
         this.frameTimer = 0;
     }
 
     update(keys, deltaTime) {
-        // Movimento horizontal
         let currentSpeed = keys.shift ? this.runSpeed : this.speed;
         
         if (keys.left) {
@@ -47,32 +42,28 @@ class Player {
             this.vx = 0;
         }
 
-        // Pulo
         if (keys.up && this.grounded) {
             this.vy = this.jumpForce;
             this.grounded = false;
         }
 
-        // Aplica física
         this.vy += this.gravity;
         this.x += this.vx;
         this.y += this.vy;
 
         this.grounded = false;
 
-        // --- CONTROLE DE ANIMAÇÃO ---
         if (!this.grounded && this.vy !== 0) {
-            this.frameY = 2; // Pulando
+            this.frameY = 2; 
             this.maxFrame = 3;
         } else if (this.vx !== 0) {
-            this.frameY = 1; // Correndo
+            this.frameY = 1; 
             this.maxFrame = 3;
         } else {
-            this.frameY = 0; // Parado
+            this.frameY = 0; 
             this.maxFrame = 2; 
         }
 
-        // --- AVANÇAR OS FRAMES ---
         if (this.frameTimer > this.frameInterval) {
             if (this.frameX < this.maxFrame) {
                 this.frameX++;
@@ -86,14 +77,11 @@ class Player {
     }
 
     draw(ctx, cameraX) {
-        // PREVENÇÃO DE ERRO: Só tenta desenhar se a imagem já tiver carregado no navegador
         if (this.image.width === 0) return;
 
-        // CÁLCULO DINÂMICO: Divide a imagem total pelo número de linhas e colunas
-        let sWidth = this.image.width / 4;  // 4 bonecos de largura
-        let sHeight = this.image.height / 3; // 3 bonecos de altura
+        let sWidth = this.image.width / 4;  
+        let sHeight = this.image.height / 3; 
 
-        // Corta na posição exata baseada no tamanho calculado
         let cropX = this.frameX * sWidth;
         let cropY = this.frameY * sHeight;
 
