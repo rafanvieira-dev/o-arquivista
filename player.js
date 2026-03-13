2b4c9069ff4138c8719947d696e58c39ce3afd3a
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 20:21:03 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,10 +1,9 @@
 class Player {
     constructor(x, y) {
         this.x = x; this.y = y;
-        this.width = 30;   
-        this.height = 72;  // Altura da gravidade
+        this.width = 30; this.height = 70; // Caixa física (Invisível)
         this.vx = 0; this.vy = 0;
-        this.speed = 5; this.jumpForce = -15.5; this.gravity = 0.8;
+        this.speed = 5; this.jumpForce = -15; this.gravity = 0.8;
         this.grounded = false; this.facing = 1; this.invincible = false;
         this.image = new Image(); this.image.src = 'assets/sprites/arquivista.png';
         this.frameX = 0; this.frameY = 0; this.frameTimer = 0;
@@ -16,7 +15,6 @@ class Player {
         else { this.vx = 0; }
 
         if (keys.up && this.grounded) { this.vy = this.jumpForce; this.grounded = false; }
-
         this.vy += this.gravity;
 
         if (!this.grounded) this.frameY = 2;
@@ -30,20 +28,24 @@ class Player {
     draw(ctx, cameraX) {
         if (this.invincible && Math.floor(Date.now() / 100) % 2) return;
         if (!this.image.complete) return;
+        
         let sWidth = this.image.width / 4;
         let sHeight = this.image.height / 3;
+        
+        // Tamanho do Desenho Vs Tamanho da Caixa de Física
+        let drawW = 60; let drawH = 80;
+        
+        // Centraliza e encosta a imagem ao fundo
+        let drawX = this.x - cameraX - (drawW - this.width) / 2;
+        let drawY = this.y - (drawH - this.height);
+
         ctx.save();
-        
-        // CÁLCULO EXATO PARA FIM DA FLUTUAÇÃO:
-        // A altura da imagem desenhada é 85, mas a física é 72. O "-13" faz os fundos alinharem.
-        let drawX = this.x - cameraX - 17;
-        let drawY = this.y - 13; 
-        
         if (this.facing === -1) {
+            ctx.translate(drawX + drawW / 2, drawY);
             ctx.scale(-1, 1);
-            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, -(drawX + 64), drawY, 64, 85);
+            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, -drawW / 2, 0, drawW, drawH);
         } else {
-            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, drawX, drawY, 64, 85);
+            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, drawX, drawY, drawW, drawH);
         }
         ctx.restore();
     }
