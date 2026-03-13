4164282d72cb5be7fad5e321bfab8561263930c2
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 20:21:24 -0300

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,12 +1,9 @@
 class Enemy {
     constructor(x, y, patrolDistance) {
         this.startX = x; this.startY = y;
-        this.x = x; this.y = y;
-        
-        this.width = 80; this.height = 80; 
-        
-        this.vx = 2.5; 
-        this.patrolDistance = patrolDistance;
+        this.width = 50; this.height = 50; // Caixa física
+        this.x = x; this.y = y; 
+        this.vx = 2; this.patrolDistance = patrolDistance;
         this.facing = 1;
         this.image = new Image(); this.image.src = 'assets/sprites/rato.png';
         this.frameX = 0; this.frameTimer = 0;
@@ -18,34 +15,35 @@ class Enemy {
             this.vx *= -1;
             this.facing = (this.vx > 0) ? 1 : -1;
         }
-
         this.frameTimer += deltaTime;
-        if (this.frameTimer > 100) {
-            this.frameX = (this.frameX + 1) % 4;
-            this.frameTimer = 0;
-        }
+        if (this.frameTimer > 100) { this.frameX = (this.frameX + 1) % 4; this.frameTimer = 0; }
     }
 
     draw(ctx, cameraX) {
         if (!this.image.complete) return;
         let sWidth = this.image.width / 4;
         let sHeight = this.image.height / 4;
+        
+        // Desenho ligeiramente maior para ficar assustador
+        let drawW = 70; let drawH = 70; 
+        let drawX = this.x - cameraX - (drawW - this.width) / 2;
+        let drawY = this.y - (drawH - this.height);
+
         ctx.save();
         if (this.facing === -1) {
+            ctx.translate(drawX + drawW / 2, drawY);
             ctx.scale(-1, 1);
-            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -(this.x - cameraX + this.width), this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -drawW / 2, 0, drawW, drawH);
         } else {
-            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, this.x - cameraX, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, drawX, drawY, drawW, drawH);
         }
         ctx.restore();
     }
 }
 
-// POSIÇÕES CORRIGIDAS: Ratos de altura 80 no chão de Y=550 ficam no Y=470!
 const enemiesList = [
-    new Enemy(600, 470, 100),  // Rato no chão 
-    new Enemy(800, 370, 150),  // Rato em cima do Armário 2 (450 - 80 = 370)
-    new Enemy(1400, 470, 150), // Rato no chão
-    new Enemy(2100, 320, 100), // Rato em cima do Armário 5 (400 - 80 = 320)
-    new Enemy(2800, 470, 150)  // Rato no chão
+    new Enemy(500, 500 - 50, 150),               // No chão principal
+    new Enemy(1450, 500 - 200 - 50, 100),        // Em cima do armário de 200 de altura
+    new Enemy(2000, 500 - 50, 150),              // No chão principal
+    new Enemy(2750, 500 - 100 - 50, 200)         // Em cima do armário de 100 de altura
 ];
