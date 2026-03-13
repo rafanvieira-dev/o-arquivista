95735f76d37017a8d22b97996546ca88919ae3ad
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 19:37:43 -0300

    Update level.js

--- level.js
+++ level.js
@@ -1,12 +1,12 @@
 const levelData = {
     platforms: [
-        { x: 0, y: 550, width: 4000, height: 50, type: 'chao' }, 
-        { x: 400, y: 350, width: 140, height: 200, type: 'armario' },
-        { x: 750, y: 450, width: 180, height: 100, type: 'armario' },
-        { x: 1100, y: 250, width: 100, height: 300, type: 'escada' },
-        { x: 1400, y: 350, width: 300, height: 40, type: 'andaime' },
-        { x: 1900, y: 200, width: 140, height: 350, type: 'armario' },
-        { x: 2300, y: 300, width: 300, height: 40, type: 'andaime' }
+        { x: 0, y: 550, width: 4000, height: 200, type: 'chao' }, 
+        { x: 0, y: 550, width: 140, height: 200, type: 'armario' },
+        { x: 0, y: 450, width: 180, height: 330, type: 'armario' },
+        { x: 0, y: 250, width: 100, height: 300, type: 'escada' },
+        { x: 0, y: 350, width: 300, height: 400, type: 'andaime' },
+        { x: 0, y: 200, width: 140, height: 600, type: 'armario' },
+        { x: 0, y: 300, width: 300, height: 500, type: 'andaime' }
     ],
     items: [
         { x: 450, y: 300, width: 35, height: 35, collected: false },
