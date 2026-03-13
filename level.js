13fba93fc25a24c147d5fec17db691bdd35e86ef
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 20:03:51 -0300

    Update level.js

--- level.js
+++ level.js
@@ -1,30 +1,24 @@
 const levelData = {
     platforms: [
-        // Chão principal expandido
+        // Chão principal (A linha do chão é exatamente no Y = 550)
         { x: 0, y: 550, width: 5000, height: 100, type: 'chao' }, 
         
-        // ZONA 1
-        { x: 400, y: 400, width: 150, height: 150, type: 'armario' }, 
-        { x: 750, y: 450, width: 250, height: 100, type: 'armario' }, 
-        
-        // ZONA 2
-        { x: 1100, y: 300, width: 100, height: 250, type: 'escada' }, 
-        { x: 1350, y: 350, width: 300, height: 40, type: 'andaime' }, 
-        
-        // ZONA 3
-        { x: 1850, y: 250, width: 150, height: 300, type: 'armario' },
-        { x: 2300, y: 350, width: 250, height: 40, type: 'andaime' },
-        { x: 2800, y: 200, width: 100, height: 350, type: 'escada' },
-        { x: 3200, y: 450, width: 200, height: 100, type: 'armario' }
+        // ARMÁRIOS (A base de todos toca perfeitamente no chão 550)
+        { x: 400, y: 400, width: 150, height: 150, type: 'armario' }, // 400 + 150 = 550
+        { x: 750, y: 450, width: 250, height: 100, type: 'armario' }, // 450 + 100 = 550
+        { x: 1200, y: 300, width: 150, height: 250, type: 'armario' },// 300 + 250 = 550
+        { x: 1600, y: 350, width: 200, height: 200, type: 'armario' },// 350 + 200 = 550
+        { x: 2100, y: 400, width: 150, height: 150, type: 'armario' },// 400 + 150 = 550
+        { x: 2500, y: 250, width: 150, height: 300, type: 'armario' },// 250 + 300 = 550
+        { x: 3000, y: 450, width: 300, height: 100, type: 'armario' } // 450 + 100 = 550
     ],
     items: [
-        // Documentos gigantes (50x50). Posição Y = Altura da plataforma - 50
+        // Documentos em cima de alguns armários
         { x: 450, y: 350, width: 50, height: 50, collected: false }, 
-        { x: 1125, y: 250, width: 50, height: 50, collected: false },
-        { x: 1475, y: 300, width: 50, height: 50, collected: false },
-        { x: 2400, y: 300, width: 50, height: 50, collected: false },
-        { x: 3250, y: 400, width: 50, height: 50, collected: false }
+        { x: 1250, y: 250, width: 50, height: 50, collected: false },
+        { x: 1650, y: 300, width: 50, height: 50, collected: false },
+        { x: 2550, y: 200, width: 50, height: 50, collected: false },
+        { x: 3100, y: 400, width: 50, height: 50, collected: false } 
     ],
-    // Portal Final!
-    finishLine: { x: 4000, y: 350, width: 80, height: 200 } 
+    finishLine: { x: 3800, y: 350, width: 80, height: 200 } 
 };
