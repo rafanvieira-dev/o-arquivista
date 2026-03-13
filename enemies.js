66435650205baf5bdab6234b22653f849c1eb307
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 19:58:21 -0300

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -2,8 +2,11 @@ class Enemy {
     constructor(x, y, patrolDistance) {
         this.startX = x; this.startY = y;
         this.x = x; this.y = y;
-        this.width = 50; this.height = 50;
-        this.vx = 2; 
+        
+        // RATOS MAIORES E MAIS ASSUSTADORES
+        this.width = 80; this.height = 80; 
+        
+        this.vx = 2.5; // Ficaram ligeiramente mais rápidos
         this.patrolDistance = patrolDistance;
         this.facing = 1;
         this.image = new Image(); this.image.src = 'assets/sprites/rato.png';
@@ -27,20 +30,22 @@ class Enemy {
     draw(ctx, cameraX) {
         if (!this.image.complete) return;
         let sWidth = this.image.width / 4;
-        let sHeight = this.image.height / 4; // Rato é 4x4
+        let sHeight = this.image.height / 4;
         ctx.save();
         if (this.facing === -1) {
             ctx.scale(-1, 1);
-            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -(this.x - cameraX + 50), this.y, 50, 50);
+            // Ajustado para desenhar o rato com o seu novo tamanho (80x80)
+            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -(this.x - cameraX + this.width), this.y, this.width, this.height);
         } else {
-            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, this.x - cameraX, this.y, 50, 50);
+            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, this.x - cameraX, this.y, this.width, this.height);
         }
         ctx.restore();
     }
 }
 
 const enemiesList = [
-    new Enemy(500, 500, 300),
-    new Enemy(1200, 500, 200),
-    new Enemy(2000, 500, 400)
+    new Enemy(600, 470, 100),  // Rato no chão
+    new Enemy(1400, 270, 150), // Rato no Andaime (350 - 80 = 270)
+    new Enemy(2000, 470, 200), // Rato no chão
+    new Enemy(2850, 470, 200)  // Rato no chão
 ];
