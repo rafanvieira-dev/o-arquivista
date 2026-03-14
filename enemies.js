181de004a848bbee591080a9e5f63eded44232da
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 22:51:55 -0300

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,7 +1,7 @@
 class Enemy {
     constructor(x, y, patrolDistance) {
         this.startX = x; this.startY = y;
-        this.width = 50; this.height = 50; 
+        this.width = 50; this.height = 30; 
         this.x = x; this.y = y; 
         this.vx = 2.5; this.patrolDistance = patrolDistance;
         this.facing = 1;
@@ -12,8 +12,7 @@ class Enemy {
     update(deltaTime) {
         this.x += this.vx;
         if (this.x > this.startX + this.patrolDistance || this.x < this.startX) {
-            this.vx *= -1;
-            this.facing = (this.vx > 0) ? 1 : -1;
+            this.vx *= -1; this.facing = (this.vx > 0) ? 1 : -1;
         }
         this.frameTimer += deltaTime;
         if (this.frameTimer > 100) { this.frameX = (this.frameX + 1) % 4; this.frameTimer = 0; }
@@ -23,14 +22,14 @@ class Enemy {
         if (!this.image.complete) return;
         let sWidth = this.image.width / 4;
         let sHeight = this.image.height / 4;
-        let drawW = 70; let drawH = 70; 
-        let drawX = this.x - cameraX - (drawW - this.width) / 2;
-        let drawY = this.y - (drawH - this.height);
+        let drawW = 70; let drawH = 50; 
+        let drawX = this.x - cameraX;
+        let drawY = this.y - 20;
+
         ctx.save();
         if (this.facing === -1) {
-            ctx.translate(drawX + drawW / 2, drawY);
-            ctx.scale(-1, 1);
-            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -drawW / 2, 0, drawW, drawH);
+            ctx.translate(drawX + drawW / 2, drawY); ctx.scale(-1, 1);
+            ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -drawW/2, 0, drawW, drawH);
         } else {
             ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, drawX, drawY, drawW, drawH);
         }
@@ -39,7 +38,6 @@ class Enemy {
 }
 
 const enemiesList = [
-    new Enemy(600, FLOOR_Y - 50, 150),
-    new Enemy(1450, FLOOR_Y - 50, 100),
-    new Enemy(2200, FLOOR_Y - 50, 200)
+    new Enemy(600, FLOOR_Y - 30, 150),
+    new Enemy(1400, FLOOR_Y - 30, 200)
 ];
