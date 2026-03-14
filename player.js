0ab639398475445ca1fffcce67ae02f51c1d515e
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-13 22:50:07 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,16 +1,26 @@
 class Player {
     constructor(x, y) {
-        this.x = x; this.y = y;
-        this.width = 30; this.height = 70;
-        this.vx = 0; this.vy = 0;
-        this.speed = 6; this.jumpForce = -15; this.gravity = 0.8;
-        this.grounded = false; this.facing = 1; this.invincible = false;
-        this.jumps = 0; this.maxJumps = 2;
+        this.x = x; 
+        this.y = y;
+        this.width = 30;  
+        this.height = 75; 
+        this.vx = 0; 
+        this.vy = 0;
+        this.speed = 6; 
+        this.jumpForce = -15; 
+        this.gravity = 0.8;
+        this.grounded = false; 
+        this.facing = 1; 
+        this.invincible = false;
+        this.jumps = 0; 
+        this.maxJumps = 2;
 
         this.image = new Image(); 
-        this.image.src = 'assets/sprites/arquivista.png';
+        this.image.src = 'assets/sprites/arquivista.png'; 
         
-        this.frameX = 0; this.frameY = 0; this.frameTimer = 0;
+        this.frameX = 0; 
+        this.frameY = 0; 
+        this.frameTimer = 0;
     }
 
     update(keys, deltaTime, jumpJustPressed) {
@@ -21,18 +31,24 @@ class Player {
         if (this.grounded) { this.jumps = 0; }
 
         if (jumpJustPressed) {
-            if (this.grounded) { this.vy = this.jumpForce; this.grounded = false; this.jumps = 1; }
-            else if (this.jumps === 1 && Math.abs(this.vx) > 0) { this.vy = this.jumpForce; this.jumps = 2; }
+            if (this.grounded) {
+                this.vy = this.jumpForce;
+                this.grounded = false;
+                this.jumps = 1;
+            } else if (this.jumps === 1 && Math.abs(this.vx) > 0) { 
+                this.vy = this.jumpForce;
+                this.jumps = 2;
+            }
         }
 
         this.vy += this.gravity;
 
-        // Escolha da Linha (Y) baseada na ação
+        // Escolha da Linha (Y) na grelha 4x4
         if (!this.grounded) this.frameY = 3; 
-        else if (this.vx !== 0) this.frameY = 2; // Andando/Correndo
-        else this.frameY = 0; // Parado
+        else if (this.vx !== 0) this.frameY = 2; 
+        else this.frameY = 0; 
 
-        // Animação apenas se estiver em movimento
+        // Animação apenas em movimento
         if (this.vx !== 0 || !this.grounded) {
             this.frameTimer += deltaTime;
             if (this.frameTimer > 90) { 
@@ -40,7 +56,7 @@ class Player {
                 this.frameTimer = 0; 
             }
         } else {
-            this.frameX = 0; // Congela parado
+            this.frameX = 0; // Congela parado no primeiro frame
         }
     }
 
@@ -51,23 +67,25 @@ class Player {
         let cellW = this.image.naturalWidth / 4;
         let cellH = this.image.naturalHeight / 4; 
         
-        // CORTE LIMPO: Ignora 2px das bordas para não ver o boneco do lado
-        let sX = (this.frameX * cellW) + 2;
-        let sY = (this.frameY * cellH) + 2;
-        let sW = cellW - 4;
-        let sH = cellH - 4;
+        // Corte suave: apenas 1px de segurança para evitar o boneco do lado
+        let sX = (this.frameX * cellW) + 1;
+        let sY = (this.frameY * cellH) + 1;
+        let sW = cellW - 2;
+        let sH = cellH - 2;
         
-        let drawW = 100; let drawH = 100;
+        let drawW = 100; 
+        let drawH = 100;
         let drawX = this.x - cameraX - (drawW - this.width) / 2;
         
-        // Ajuste exato para pisar na madeira (540px)
-        let ajusteDosPes = 26; 
+        // Ajuste para pisar na madeira (560px)
+        let ajusteDosPes = 28; 
         let drawY = this.y - (drawH - this.height) + ajusteDosPes;
 
         ctx.save();
         if (this.facing === -1) {
-            ctx.translate(drawX + drawW / 2, drawY); ctx.scale(-1, 1);
-            ctx.drawImage(this.image, sX, sY, sW, sH, -drawW/2, 0, drawW, drawH);
+            ctx.translate(drawX + drawW / 2, drawY);
+            ctx.scale(-1, 1);
+            ctx.drawImage(this.image, sX, sY, sW, sH, -drawW / 2, 0, drawW, drawH);
         } else {
             ctx.drawImage(this.image, sX, sY, sW, sH, drawX, drawY, drawW, drawH);
         }
