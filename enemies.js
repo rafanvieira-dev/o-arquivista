8a704a0b83ab293e375d66691ec6befdceca28c6
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-14 10:27:26 -0300

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,25 +1,19 @@
 class Enemy {
     constructor(x, y, patrolDistance) {
-        this.startX = x; 
-        this.startY = y;
-        this.width = 65; 
-        this.height = 40; 
-        this.x = x; 
-        this.y = y; 
-        this.vx = 3.5; 
-        this.patrolDistance = patrolDistance;
+        this.startX = x; this.startY = y;
+        this.width = 65; this.height = 40; 
+        this.x = x; this.y = y; 
+        this.vx = 3.5; this.patrolDistance = patrolDistance;
         this.facing = 1;
         this.image = new Image(); 
         this.image.src = 'assets/sprites/rato.png';
-        this.frameX = 0; 
-        this.frameTimer = 0;
+        this.frameX = 0; this.frameTimer = 0;
     }
 
     update(deltaTime) {
         this.x += this.vx;
         if (this.x > this.startX + this.patrolDistance || this.x < this.startX) {
-            this.vx *= -1; 
-            this.facing = (this.vx > 0) ? 1 : -1;
+            this.vx *= -1; this.facing = (this.vx > 0) ? 1 : -1;
         }
         this.frameTimer += deltaTime;
         if (this.frameTimer > 60) { 
@@ -32,16 +26,13 @@ class Enemy {
         if (!this.image.complete || this.image.naturalWidth === 0) return;
         let sWidth = this.image.width / 4;
         let sHeight = this.image.height / 4;
-        
-        let drawW = 100; 
-        let drawH = 65; 
+        let drawW = 100; let drawH = 65; 
         let drawX = this.x - cameraX - (drawW - this.width) / 2;
         let drawY = this.y - (drawH - this.height);
 
         ctx.save();
         if (this.facing === -1) {
-            ctx.translate(drawX + drawW / 2, drawY); 
-            ctx.scale(-1, 1);
+            ctx.translate(drawX + drawW / 2, drawY); ctx.scale(-1, 1);
             ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, -drawW/2, 0, drawW, drawH);
         } else {
             ctx.drawImage(this.image, this.frameX * sWidth, 0, sWidth, sHeight, drawX, drawY, drawW, drawH);
