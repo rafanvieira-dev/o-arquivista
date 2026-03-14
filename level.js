152b03a0b24a2b25589921c461a644a70c08619d
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-14 12:21:19 -0300

    Update level.js

--- level.js
+++ level.js
@@ -1,9 +1,9 @@
 const FLOOR_Y = 560; 
 
 function generateLevel(levelNumber) {
-    // bgW é aproximadamente a largura do fundo na tela após o corte
+    // Largura visual exata do cenário após o corte no ecrã 4:3
     const bgW = 941; 
-    const numChunks = 2 + levelNumber; // O tamanho da fase aumenta com os níveis
+    const numChunks = 2 + levelNumber; // Vai adicionando mais frames ao longo das fases
     const levelLength = numChunks * bgW; 
     
     let backgroundToUse = `assets/sprites/fundo${levelNumber}.png`;
@@ -12,29 +12,28 @@ function generateLevel(levelNumber) {
     let items = [];
     let enemies = [];
 
-    // LÓGICA NOVA: Para cada frame de cenário, colocamos os armários partindo de pontos fixos no meio!
+    // LÓGICA DE DISTRIBUIÇÃO SEGURA (NUNCA nas divisas das imagens)
     for (let i = 0; i < numChunks; i++) {
         let chunkStart = i * bgW;
 
-        // ARMÁRIO 1: Começa SEMPRE aos 250px após o início do frame (Longe das divisas)
+        // Armário 1: No meio-início do frame (aos 250px)
         let arm1X = chunkStart + 250;
         let arm1W = 120 + Math.random() * (20 + levelNumber * 10);
-        let arm1H = 80 + Math.random() * (30 + levelNumber * 15); // Apenas altura e largura mudam
+        let arm1H = 80 + Math.random() * (30 + levelNumber * 15); 
         platforms.push({ x: arm1X, y: FLOOR_Y - arm1H, width: arm1W, height: arm1H, type: 'armario' });
 
-        // Documento no Armário 1
         let docSize = 70;
         if (Math.random() > 0.3) {
             items.push({ x: arm1X + (arm1W/2) - (docSize/2), y: FLOOR_Y - arm1H - docSize - 5, width: docSize, height: docSize, collected: false });
         }
 
-        // Inimigo no chão, no espaço livre entre os armários
+        // Rato solto no chão no espaço morto do meio
         let enemyChance = 0.2 + (levelNumber * 0.1); 
         if (Math.random() < enemyChance) {
             enemies.push(new Enemy(chunkStart + 450, FLOOR_Y - 40, 100));
         }
 
-        // ARMÁRIO 2: Começa SEMPRE aos 650px após o início do frame (Nunca na divisa)
+        // Armário 2: No meio-fim do frame (aos 650px)
         let arm2X = chunkStart + 650;
         let arm2W = 120 + Math.random() * (30 + levelNumber * 10);
         let arm2H = 90 + Math.random() * (30 + levelNumber * 15);
