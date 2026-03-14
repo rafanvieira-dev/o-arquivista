9cf065fbf7f1cad36ad9072312dc07919b9f3db7
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-14 08:37:18 -0300

    Update level.js

--- level.js
+++ level.js
@@ -1,4 +1,5 @@
-const FLOOR_Y = 560; 
+// O chão agora fica mais abaixo porque a parte preta foi removida!
+const FLOOR_Y = 580; 
 
 function generateLevel(levelNumber) {
     const levelLength = 2500 + (levelNumber * 1000); 
@@ -12,26 +13,21 @@ function generateLevel(levelNumber) {
     let currentX = 400;
 
     while (currentX < levelLength - 500) {
-        // Aumenta a distância (buracos maiores) conforme o nível sobe
         let gap = 120 + Math.random() * (100 + levelNumber * 15);
         currentX += gap;
 
-        // Armários ficam mais altos conforme o nível sobe!
         let armW = 120 + Math.random() * 120;
         let armH = 80 + Math.random() * (80 + levelNumber * 15);
 
         platforms.push({ x: currentX, y: FLOOR_Y - armH, width: armW, height: armH, type: 'armario' });
 
-        // Documentos maiores (60x60)
         let docSize = 60;
         if (Math.random() > 0.3) {
             items.push({ x: currentX + (armW/2) - (docSize/2), y: FLOOR_Y - armH - docSize - 5, width: docSize, height: docSize, collected: false });
         }
 
-        // Mais ratos nas fases difíceis! O "enemyChance" cresce muito.
         let enemyChance = 0.3 + (levelNumber * 0.1); 
         if (Math.random() < enemyChance && gap > 100) {
-            // Rato nasce no chão (FLOOR_Y - altura do rato [40])
             enemies.push(new Enemy(currentX - gap + 20, FLOOR_Y - 40, gap - 40));
         }
 
@@ -43,6 +39,6 @@ function generateLevel(levelNumber) {
         items: items,
         enemies: enemies,
         finishLine: { x: levelLength, y: FLOOR_Y - 300, width: 100, height: 300 },
-        timeLimit: 80 + (levelNumber * 30) // Mais tempo para níveis gigantes
+        timeLimit: 80 + (levelNumber * 30) 
     };
 }
