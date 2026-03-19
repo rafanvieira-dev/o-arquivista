4f8bfe51e7be81ecfd62bece82d780623602e0b3
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-19 00:14:43 -0300

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -6,7 +6,7 @@ class Enemy {
         this.y = y; 
         this.patrolDistance = patrolDistance;
         this.facing = 1;
-        this.type = type; 
+        this.type = type; // 'ground' ou 'flying'
         this.image = new Image(); 
         
         // Offset de tempo aleatório para o voo não ser igual em todas
@@ -15,15 +15,17 @@ class Enemy {
         if (this.type === 'ground') {
             this.image.src = 'assets/sprites/rato.png';
             this.numFramesX = 4; 
-            this.numFramesY = 1; 
+            
+            // O GRANDE MISTÉRIO RESOLVIDO: O rato também é uma grelha 4x4 e não 4x1!
+            this.numFramesY = 4; 
             this.frameY = 0; 
             
-            // Hitbox e tamanho de desenho ajustados para não achatar
+            // Tamanho natural
             this.width = 60; 
             this.height = 35; 
             this.drawW = 85;  
             this.drawH = 55;  
-            this.vx = 2.5; // Velocidade do rato
+            this.vx = 2.5; 
             
         } else if (this.type === 'flying') {
             this.image.src = 'assets/sprites/barata_voadora.png'; 
@@ -31,12 +33,11 @@ class Enemy {
             this.numFramesY = 4;
             this.frameY = 0; 
             
-            // Hitbox e tamanho de desenho QUADRADOS para a barata não ficar esmagada
             this.width = 50; 
             this.height = 50; 
             this.drawW = 80; 
             this.drawH = 80; 
-            this.vx = 4; // Barata voa mais rápido
+            this.vx = 4; 
         }
 
         this.frameX = 0; 
@@ -44,7 +45,6 @@ class Enemy {
     }
 
     update(deltaTime) {
-        // Movimento Horizontal
         this.x += this.vx;
         if (this.x > this.startX + this.patrolDistance || this.x < this.startX) {
             this.vx *= -1; 
@@ -54,16 +54,20 @@ class Enemy {
         this.frameTimer += deltaTime;
         
         if (this.type === 'ground') {
-            // Animação do Rato
+            // Animação do Rato (ciclo pelos 16 frames da grelha 4x4)
             if (this.frameTimer > 60) { 
-                this.frameX = (this.frameX + 1) % 4; 
+                this.frameX++;
+                if (this.frameX >= 4) {
+                    this.frameX = 0;
+                    this.frameY = (this.frameY + 1) % 4;
+                }
                 this.frameTimer = 0; 
             }
         } else if (this.type === 'flying') {
-            // Efeito flutuante (sobe e desce suavemente)
+            // Flutuação suave para a barata
             this.y = this.startY + Math.sin((Date.now() / 200) + this.timeOffset) * 15;
 
-            // Animação da Barata (Bate asas muito mais rápido)
+            // Animação rápida de voo (ciclo de 16 frames)
             if (this.frameTimer > 35) { 
                 this.frameX++;
                 if (this.frameX >= 4) {
@@ -78,8 +82,9 @@ class Enemy {
     draw(ctx, cameraX) {
         if (!this.image.complete || this.image.naturalWidth === 0) return;
 
-        let sWidth = this.image.naturalWidth / this.numFramesX;
-        let sHeight = this.image.naturalHeight / this.numFramesY;
+        // Agora o corte usa 4x4 para ambos, rato e barata
+        let cellWidth = this.image.naturalWidth / this.numFramesX;
+        let cellHeight = this.image.naturalHeight / this.numFramesY;
         
         let drawX = this.x - cameraX - (this.drawW - this.width) / 2;
         let drawY = this.y - (this.drawH - this.height);
@@ -88,20 +93,18 @@ class Enemy {
         if (this.facing === -1) {
             ctx.translate(drawX + this.drawW / 2, drawY); 
             ctx.scale(-1, 1);
-            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, -this.drawW/2, 0, this.drawW, this.drawH);
+            ctx.drawImage(this.image, this.frameX * cellWidth, this.frameY * cellHeight, cellWidth, cellHeight, -this.drawW/2, 0, this.drawW, this.drawH);
         } else {
-            ctx.drawImage(this.image, this.frameX * sWidth, this.frameY * sHeight, sWidth, sHeight, drawX, drawY, this.drawW, this.drawH);
+            ctx.drawImage(this.image, this.frameX * cellWidth, this.frameY * cellHeight, cellWidth, cellHeight, drawX, drawY, this.drawW, this.drawH);
         }
         ctx.restore();
     }
 }
 
-// A classe NPC continua igual abaixo:
 class NPC {
     constructor(x, y, type) {
         this.x = x; this.y = y; this.type = type;
         this.width = 30; this.height = 75; 
-        
         if (type === 'flavio') { this.image = assets.flavio; this.message = "Excelente trabalho, Arquivista!"; }
         else if (type === 'rosale') { this.image = assets.rosale; this.message = "Obrigada por trazer os arquivos!"; }
         else if (type === 'eliezer') { this.image = assets.eliezer; this.message = "Os registos estão seguros aqui."; }
@@ -115,7 +118,6 @@ class NPC {
         let drawX = Math.floor(this.x - cameraX - (drawW - this.width) / 2);
         let drawY = Math.floor((this.y + this.height) + 18 - drawH); 
         ctx.drawImage(this.image, 0, 0, sW, sH, drawX, drawY, drawW, drawH);
-
         if (drawX > -100 && drawX < 900) {
             let bubbleW = 280; let bubbleH = 30;
             let bubbleX = drawX + (drawW / 2) - (bubbleW / 2); let bubbleY = drawY - bubbleH - 25;
